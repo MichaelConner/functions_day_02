a4c74990eaa1e782700bb90b280f627271fb000e
Michael Conner <michaelconner@hotmail.co.uk>
2018-11-20 19:29:59 +0000

    function #7 & #8 added

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -21,3 +21,11 @@ end
 def length_of_string (string)
   return string.length
 end
+
+def join_string (string_1, string_2)
+  return "#{string_1}#{string_2}"
+end
+
+def add_string_as_number(string_num1, string_num2)
+  return string_num1.to_f + string_num2.to_f
+end
