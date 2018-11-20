2a79ddf8503d1bd8788ca365af25a986d0532b04
Gabriele Filigheddu <gabriele.filigheddu@hotmail.com>
2018-11-20 17:04:20 +0000

    Initial commit

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,19 @@
 def return_10
   return 10
 end
+
+def add (num1, num2)
+  return num1 + num2
+end
+
+def subtract (num1, num2)
+  return num1 - num2
+end
+
+def multiply (num1, num2)
+  return num1 * num2
+  end
+
+def divide (num1, num2)
+  return num1 / num2
+end
